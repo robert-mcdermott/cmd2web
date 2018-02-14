f6945039ecee8811905ff1943344c48b6fef04af
Robert McDermott <robert.c.mcdermott@gmail.com>
2018-02-14 14:03:19 -0800

    added SSL and basic authentication

--- main.go
+++ main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-        "fmt"
+	"fmt"
 	"log"
 	"math/rand"
-        "net"
+	"net"
 	"os"
 	"os/exec"
 	"time"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/auth"
 )
 
 var cmd []string
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
+var user, pass string
 
 func RandString(n int) string {
-        chars := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = chars[rand.Intn(len(chars))]
@@ -33,7 +31,7 @@ type MainController struct {
 }
 
 func (this *MainController) Get() {
-	this.Ctx.WriteString(string("nice try!"))
+	this.Ctx.WriteString(string("That's not the secret string!"))
 }
 
 type CmdController struct {
@@ -47,7 +45,6 @@ func (this *CmdController) Get() {
 	}
 	this.Ctx.WriteString(string(out))
 }
-
 
 func GetFreePort() (int, error) {
 	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
@@ -63,18 +60,47 @@ func GetFreePort() (int, error) {
 	return l.Addr().(*net.TCPAddr).Port, nil
 }
 
+func SecretAuth(username, password string) bool {
+	if username == user && password == pass {
+		return true
+	}
+	return false
+}
+
 func main() {
 	cmd = os.Args[1:]
+	user = "cmd2web"
+	pass = RandString(8)
 	path := RandString(30)
-        port, err := GetFreePort()
-        if err != nil {
-            log.Fatal(err)
+	port, err := GetFreePort()
+	if err != nil {
+		log.Fatal(err)
 	}
-        fmt.Printf("http://<this-server>:%d/%s\n", port, path)
-        beego.BConfig.RunMode = "prod"
-	beego.BConfig.Listen.HTTPPort = port 
-        beego.BConfig.WebConfig.DirectoryIndex = true 
-        beego.SetStaticPath(fmt.Sprintf("/%s/files", path),".")
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("\nURL: https://%s:%d/%s\n", hostname, port, path)
+	fmt.Printf("Username: %s\n", user)
+	fmt.Printf("Password: %s\n\n", pass)
+	beego.BConfig.RunMode = "prod"
+	beego.BConfig.Listen.EnableHTTP = false
+	//beego.BConfig.Listen.HTTPPort = port
+	beego.BConfig.Log.AccessLogs = true
+	beego.BConfig.Listen.EnableHTTPS = true
+	beego.BConfig.Listen.HTTPSPort = port
+	beego.BConfig.Listen.HTTPSCertFile = "ssl/cert.pem"
+	beego.BConfig.Listen.HTTPSKeyFile = "ssl/cert.key"
+	beego.BConfig.WebConfig.DirectoryIndex = true
+	authPlugin := auth.NewBasicAuthenticator(SecretAuth, "cmd2web")
+	//beego.InsertFilter("*", beego.BeforeRouter, authPlugin)
+	//beego.InsertFilter("*", beego.BeforeExec, authPlugin)
+	//Tried the above two variations, but the "beego.BeforeStatic" is the only
+	//one the that will prompt for password for static content (files in this case)
+	beego.InsertFilter("*", beego.BeforeStatic, authPlugin)
+	beego.SetStaticPath(fmt.Sprintf("/%s/files", path), ".")
 	beego.Router(path, &CmdController{})
 	beego.Router("/*", &MainController{})
 	beego.Run()
